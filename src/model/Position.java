b21aaa33c0aebe8c82e4095e98dd9214d221f2b4
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:04:03 +0800

    Create Position.java

---
+++ src/model/Position.java
@@ -0,0 +1,27 @@
+public class Position {
+    private int row, col;
+
+    public Position(int r, int c) {
+        row = r;
+        col = c;
+        }
+    public int getRow() {
+        return row; 
+        }
+    public int getCol() { 
+        return col; 
+        }
+    public void setRow(int r) { 
+        row = r; 
+        }
+    public void setCol(int c) {
+        col = c; 
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Position)) return false;
+        Position other = (Position) o;
+        return row == other.row && col == other.col;
+    }
+}
