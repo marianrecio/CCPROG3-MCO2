2ce91f0d53093debf61e5e998b560cae1301b7b2
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:35:20 +0800

    Create Entrance.java

---
+++ src/model/Entrance.java
@@ -0,0 +1,11 @@
+public class Entrance implements Amenity {
+    public void interact(Shopper s, Supermarket m) {
+        System.out.println("At entrance.");
+    }
+    public char getSymbol() { 
+        return '^'; 
+    }
+    public boolean isPassable() { 
+        return true; 
+    }
+}
