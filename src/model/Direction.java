0b843563e8e1f11bfab742a48b30c85c2914a378
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:01:00 +0800

    Create Direction.java

---
+++ src/model/Direction.java
@@ -0,0 +1,18 @@
+public enum Direction {
+    NORTH, SOUTH, EAST, WEST;
+
+    public static Direction direction(char c) {
+        switch (c) {
+            case 'I': 
+              return NORTH;
+            case 'K': 
+              return SOUTH;
+            case 'J': 
+              return WEST;
+            case 'L': 
+              return EAST;
+            default: 
+              return NORTH;
+        }
+    }
+}
