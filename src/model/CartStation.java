fa6618e7fbf6a6c9e87494cba82a316078dadbce
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:11:22 +0800

    Create CartStation.java

---
+++ src/model/CartStation.java
@@ -0,0 +1,12 @@
+public class CartStation implements Amenity {
+    public void interact(Shopper shopper, Supermarket market) {
+        shopper.equipCart();
+    }
+
+    public char getSymbol() { 
+        return 'R'; 
+    }
+    public boolean isPassable() { 
+        return true; 
+    }
+}
