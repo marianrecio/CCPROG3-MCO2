4d2a64ed0b9baa2d9c068941341fbb7c4fee6e5e
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:34:01 +0800

    Create CheckoutCounter.java

---
+++ src/model/CheckoutCounter.java
@@ -0,0 +1,13 @@
+public class CheckoutCounter implements Amenity {
+    private Checkout checkout = new Checkout();
+
+    public void interact(Shopper s, Supermarket m) {
+        checkout.interact(s, m);
+    }
+    public char getSymbol() { 
+       return 'C'; 
+    }
+    public boolean isPassable() { 
+      return false; 
+    }
+}
