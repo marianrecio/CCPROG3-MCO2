3135b528efa71d389fbcfe6d288822f6caa214a7
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:13:01 +0800

    Create Service.java

---
+++ src/model/Service.java
@@ -0,0 +1,11 @@
+public abstract class Service implements Amenity {
+    protected String name;
+
+    public Service(String name) {
+        this.name = name;
+    }
+
+    public boolean isPassable() { 
+        return true; 
+    }
+}
