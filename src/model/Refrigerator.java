ce1301fe2bf1b3bcb387c77de86e78e846551f8b
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:32:56 +0800

    Create Refrigerator.java

---
+++ src/model/Refrigerator.java
@@ -0,0 +1,11 @@
+public class Refrigerator implements Amenity {
+    public void interact(Shopper s, Supermarket m) {
+        System.out.println("Refrigerator.");
+    }
+    public char getSymbol() { 
+        return 'F'; 
+    }
+    public boolean isPassable() { 
+        return false; 
+    }
+}
