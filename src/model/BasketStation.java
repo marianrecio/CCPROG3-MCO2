e708531c3bdef6d540de8a2dbd142baa8daa4dda
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 20:10:50 +0800

    Create BasketStation.java

---
+++ src/model/BasketStation.java
@@ -0,0 +1,12 @@
+public class BasketStation implements Amenity {
+    public void interact(Shopper shopper, Supermarket market) {
+        shopper.equipBasket();
+    }
+
+    public char getSymbol() { 
+        return 'B'; 
+    }
+    public boolean isPassable() { 
+        return true; 
+    }
+}
