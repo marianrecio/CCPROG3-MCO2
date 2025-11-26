a03f11c5f4643fc70469c7a79bbff8a55d18009b
marianrecio <marian_recio@dlsu.edu.ph>
2025-11-26 19:57:18 +0800

    Update Amenity.java

--- src/model/Amenity.java
+++ src/model/Amenity.java
@@ -3,21 +3,3 @@ interface Amenity {
     char getSymbol();
     boolean isPassable();
 }
-
-class BasketStation implements Amenity {
-    public void interact(Shopper shopper, Supermarket market) {
-        shopper.equipBasket();
-    }
-
-    public char getSymbol() { return 'B'; }
-    public boolean isPassable() { return true; }
-}
-
-class CartStation implements Amenity {
-    public void interact(Shopper shopper, Supermarket market) {
-        shopper.equipCart();
-    }
-
-    public char getSymbol() { return 'R'; }
-    public boolean isPassable() { return true; }
-}
