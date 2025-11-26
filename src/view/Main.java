2796eeb6089ae9aa9fbf7a3fbdb18d1490971ada
mikoxancheta <miko_ancheta@dlsu.edu.ph>
2025-11-26 22:34:14 +0800

    Update Main.java

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,11 +1,24 @@
+package view;
+
+import controller.SupermarketSimulatorController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         SupermarketView view = new SupermarketView();
+        new SupermarketSimulatorController(view); // attach controller
+
         Scene scene = new Scene(view.getRoot());
         stage.setScene(scene);
         stage.setTitle("Supermarket Simulator");
         stage.show();
+
+        // Important: request focus so WASD keys work
+        view.getRoot().requestFocus();
     }
 
     public static void main(String[] args) {
